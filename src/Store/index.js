fa7d917e49f9b59722b37e1a94b899a3f8e27b29
briantical <lutbrianivan@gmail.com>
2021-09-18 01:51:53 +0300

    chore(plantit): setup folder structure, dependencies and configuration

---
+++ src/Store/index.js
@@ -0,0 +1,6 @@
+import * as contexts from './context';
+import * as initialStates from './initialState';
+import * as reducers from './reducers';
+import * as types from './types';
+
+export { contexts, initialStates, reducers, types };
